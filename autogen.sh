d52bd686fe91e4c5104b60632ed18eb26ae827f6
Steve M. Robbins <smr@sumost.ca>
2001-06-20 13:17:37 +0000

    Add README.CVS; improve autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,37 +1,11 @@
 #!/bin/sh
 
-DIE=0
+set -e
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`autoconf' installed ."
-  DIE=1
-}
+ACLOCAL_FLAGS="-I config"
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`automake' installed."
-  DIE=1
-  NO_AUTOMAKE=yes
-}
-
-(aclocal --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'."
-  DIE=1
-}
-
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`libtool' installed."
-  DIE=1
-}
-
-
-if [ -d /usr/local/share/aclocal ]; then
-  ACLOCAL_FLAGS="-I /usr/local/share/aclocal $ACLOCAL_FLAGS"
-fi
-aclocal -I config $ACLOCAL_FLAGS 
+aclocal $ACLOCAL_FLAGS  || \
+    aclocal $ACLOCAL_FLAGS -I /usr/local/share/aclocal
 libtoolize --force 
 autoheader
 automake --add-missing
