5db9f86a3c08232a0b8062ef81e8a48a5c0e9bc4
Baptiste Lepilleur <gaiacrtn@free.fr>
2006-06-29 17:45:37 +0000

    autogen.sh: applied patch #1449380 to allow running autogen on Mac OS X.

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,16 @@ ACLOCAL_FLAGS="-I config"
 
 aclocal $ACLOCAL_FLAGS  || \
     aclocal $ACLOCAL_FLAGS -I /usr/local/share/aclocal
-libtoolize --force 
+
+for l in libtoolize glibtoolize;
+do
+ ($l --version) < /dev/null > /dev/null 2>&1 && {
+ LIBTOOLIZE=$l
+ break
+ }
+done
+
+$LIBTOOLIZE --force
 autoheader
 automake --add-missing
 autoconf
