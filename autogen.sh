1059029fac4b27fd97008337e97f7ed2c502fdc2
Tomas Chvatal <tchvatal@suse.cz>
2012-03-13 13:55:50 +0100

    Add new autoconf using the latest macros
    This add new macros from autoconf-archive instead of the old
    broken ones. Also cleans up the configure.in and moves it to
    configure.ac.
    
    This setup as-is is not working. Next step is checking config.h
    to contain same contend for before/after update.

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,56 @@
 #!/bin/sh
+TESTLIBTOOLIZE="glibtoolize libtoolize"
 
-set -e
+LIBTOOLIZEFOUND="0"
 
-ACLOCAL_FLAGS="-I config"
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-aclocal $ACLOCAL_FLAGS  || \
-    aclocal $ACLOCAL_FLAGS -I /usr/local/share/aclocal
+olddir=`pwd`
+cd $srcdir
 
-for l in libtoolize glibtoolize;
-do
- ($l --version) < /dev/null > /dev/null 2>&1 && {
- LIBTOOLIZE=$l
- break
- }
+ACLOCAL_FLAGS="-I m4"
+
+aclocal --version > /dev/null 2> /dev/null || {
+	echo "error: aclocal not found"
+	exit 1
+}
+automake --version > /dev/null 2> /dev/null || {
+	echo "error: automake not found"
+	exit 1
+}
+
+for i in $TESTLIBTOOLIZE; do
+	if which $i > /dev/null 2>&1; then
+		LIBTOOLIZE=$i
+		LIBTOOLIZEFOUND="1"
+		break
+	fi
 done
 
-$LIBTOOLIZE --force
-autoheader
-automake --add-missing
-autoconf
+if [ "$LIBTOOLIZEFOUND" = "0" ]; then
+	echo "$0: need libtoolize tool to build libvisio" >&2
+	exit 1
+fi
+
+rm -rf autom4te*.cache
+
+$LIBTOOLIZE --force --copy || {
+	echo "error: libtoolize failed"
+	exit 1
+}
+aclocal $ACLOCAL_FLAGS || {
+	echo "error: aclocal $ACLOCAL_FLAGS failed"
+	exit 1
+}
+autoheader || {
+	echo "error: autoheader failed"
+	exit 1
+}
+automake -a -c --foreign || {
+	echo "warning: automake failed"
+}
+autoconf || {
+	echo "error: autoconf failed"
+	exit 1
+}
