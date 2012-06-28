3a1718e0109e9cef4f848bcdbe0c5d92e06e0830
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-06-28 10:11:41 +0200

    Some build system tweaks

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,54 @@
 #!/bin/sh
+TESTLIBTOOLIZE="glibtoolize libtoolize"
 
-set -e
+LIBTOOLIZEFOUND="0"
 
-ACLOCAL_FLAGS="-I config"
+aclocal --version > /dev/null 2> /dev/null || {
+    echo "error: aclocal not found"
+    exit 1
+}
+automake --version > /dev/null 2> /dev/null || {
+    echo "error: automake not found"
+    exit 1
+}
 
-aclocal $ACLOCAL_FLAGS  || \
-    aclocal $ACLOCAL_FLAGS -I /usr/local/share/aclocal
-
-for l in libtoolize glibtoolize;
-do
- ($l --version) < /dev/null > /dev/null 2>&1 && {
- LIBTOOLIZE=$l
- break
- }
+for i in $TESTLIBTOOLIZE; do
+	if which $i > /dev/null 2>&1; then
+		LIBTOOLIZE=$i
+		LIBTOOLIZEFOUND="1"
+		break
+	fi
 done
 
-$LIBTOOLIZE --force
-autoheader
-automake --add-missing
-autoconf
+if [ "$LIBTOOLIZEFOUND" = "0" ]; then
+	echo "$0: need libtoolize tool to build cppunit" >&2
+	exit 1
+fi
+
+amcheck=`automake --version | grep 'automake (GNU automake) 1.5'`
+if test "x$amcheck" = "xautomake (GNU automake) 1.5"; then
+    echo "warning: you appear to be using automake 1.5"
+    echo "         this version has a bug - GNUmakefile.am dependencies are not generated"
+fi
+
+rm -rf autom4te*.cache
+
+$LIBTOOLIZE --force --copy || {
+    echo "error: libtoolize failed"
+    exit 1
+}
+aclocal $ACLOCAL_FLAGS -I config || {
+    echo "error: aclocal $ACLOCAL_FLAGS failed"
+    exit 1
+}
+autoheader || {
+    echo "error: autoheader failed"
+    exit 1
+}
+automake -a -c --foreign || {
+    echo "warning: automake failed"
+}
+autoconf || {
+    echo "error: autoconf failed"
+    exit 1
+}
