442300567b57ff6a02b180e36407b93e2177a3fc
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-10-15 17:45:56 +0200

    always use std::unique_ptr

--- include/cppunit/portability/SmartPtr.h
+++ include/cppunit/portability/SmartPtr.h
@@ -1,10 +1,6 @@
 #ifndef CPPUNIT_PORTABILITY_CPPUNITSMARTPTR_H
 #define CPPUNIT_PORTABILITY_CPPUNITSMARTPTR_H
 
-#if HAVE_CXX11
 #define CppUnitSmartPtr std::unique_ptr
-#else
-#define CppUnitSmartPtr std::auto_ptr
-#endif
 
 #endif // CPPUNIT_PORTABILITY_CPPUNITDEQUE_H
